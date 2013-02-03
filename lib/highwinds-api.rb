d4b675e9a0af6a515c3dc7444a08076cff3fb7c7
Ofir Kerker <ofir@crossrider.com>
2013-02-03 14:06:55 +0200

    First commit

---
+++ lib/highwinds-api.rb
@@ -0,0 +1,7 @@
+require "highwinds-api/version"
+
+module Highwinds
+  module Api
+    # Your code goes here...
+  end
+end
---
+++ lib/highwinds-api/version.rb
@@ -0,0 +1,5 @@
+module Highwinds
+  module Api
+    VERSION = "0.0.1"
+  end
+end
