38b6277ad9b29bd80802faad71e95a2e8a890e20
Ofir Kerker <ofir@crossrider.com>
2013-02-10 20:00:44 +0200

    Add Purge by path

--- lib/highwinds-api/content.rb
+++ lib/highwinds-api/content.rb
@@ -2,16 +2,24 @@ module HighwindsAPI
   require 'httparty'
   class Content
     include HTTParty
-  
+    # debug_output $stdout # adds HTTP debugging 
+
     base_uri 'https://striketracker2.highwinds.com/webservices/content/'
-    
+
     def self.purge_url(url, recursive)
       options = {
-          :headers    =>  { 'Content-Type'  => 'application/xml', 
-                            'Accept'        => 'application/xml' },
-          :basic_auth =>  HighwindsAPI.credentials 
-      }
+      :headers    =>  { 'Content-Type'  => 'application/xml', 
+                        'Accept'        => 'application/xml' },
+      :basic_auth =>  HighwindsAPI.credentials }
       self.delete("?recursive=#{recursive}&url=#{url}", options)
+    end
+
+    def self.purge_path(host_hash, path)
+      options = {
+      :headers    =>  { 'Content-Type'  => 'application/xml', 
+                        'Accept'        => 'application/xml' },
+      :basic_auth =>  HighwindsAPI.credentials }
+      self.delete("/#{host_hash}/cds/#{path}", options)
     end
 
   end
--- spec/content_spec.rb
+++ spec/content_spec.rb
@@ -3,17 +3,29 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'highwin
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'highwinds-api/content'))
 
 describe 'HighwindsAPI::Content' do
-  username = "<Your username>"
-  password = "<Your password>"
+  username = "<YouUserName>"
+  password = "<YourPassword>"
   it "should not purge content, if username and password are not correct." do
-    HighwindsAPI.set_credentials(username, password)
+    HighwindsAPI.set_credentials("bad username", "bad password")
     r = HighwindsAPI::Content.purge_url("http://staging.crossrider.com/kerker/", true).response
     r.code.should eq("403"), "response was: #{r}"
   end
 
-  xit "should purge content" do
+  it "should purge content by url" do
     HighwindsAPI.set_credentials(username, password)
     r = HighwindsAPI::Content.purge_url("http://staging.crossrider.com/kerker/", true).response
+    r.code.should eq("200"), "response was: #{r}"
+  end
+
+  it "should purge folder by path" do
+    HighwindsAPI.set_credentials(username, password)
+    r = HighwindsAPI::Content.purge_path("y2s9x4y9", "kerker/").response
+    r.code.should eq("200"), "response was: #{r}"
+  end
+
+  it "should purge file by path" do
+    HighwindsAPI.set_credentials(username, password)
+    r = HighwindsAPI::Content.purge_path("y2s9x4y9", "kerker/akamai").response
     r.code.should eq("200"), "response was: #{r}"
   end
 
